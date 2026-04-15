6f0824f4f5276c810b09d37756a7cfad77783754
Muneeb Khan <Muneebkhn0786@gmail.com>
2026-04-16 01:41:18 +0530

    code fixes

--- src/components/common/logo.jsx
+++ src/components/common/logo.jsx
@@ -2,8 +2,6 @@ import React from "react";
 import { Link } from "react-router-dom";
 import hello from '../../assets/hi-removebg-preview.png';
 
-import INFO from "../../data/user";
-
 import "./styles/logo.css";
 
 const Logo = (props) => {
